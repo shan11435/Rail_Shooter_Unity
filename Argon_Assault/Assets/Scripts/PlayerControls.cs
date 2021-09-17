fa0b91fa3da48252a8cb70445ff5219951be6ec0
Tahsin Ahmed <shanahmed1997@gmail.com>
2021-09-17 17:44:02 -0400

    currently making the ship rotate

--- Argon_Assault/Assets/Scripts/PlayerControls.cs
+++ Argon_Assault/Assets/Scripts/PlayerControls.cs
@@ -6,8 +6,21 @@ public class PlayerControls : MonoBehaviour
 {
     //this allows you to control how fast the player moves in unity editor
     [SerializeField] float controlSpeed = 10f;
-    // Update is called once per frame
+    [SerializeField] private float xRange = 5f;
+    [SerializeField] private float yRange = 5f;
+    [SerializeField] private float negYRange = -7f;
     void Update()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        transform.localRotation = Quaternion.Euler(-30f, 30f, 0f);
+    }
+
+    private void ProcessTranslation()
     {
         //this makes the player move when pressing left or right key or A or D key
         float xThrow = Input.GetAxis("Horizontal");
@@ -17,15 +30,19 @@ public class PlayerControls : MonoBehaviour
         //this line makes the player move left or right when pressing the xThrow keys
         float xOffset = xThrow * Time.deltaTime * controlSpeed;
         //transform.localPosition means where you currently are in the world
-        float newXPos = transform.localPosition.x + xOffset;
+        float rawXPos = transform.localPosition.x + xOffset;
+        //this will cause the rocket to not go off the screen
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
         //this line makes the player move up or down when pressing the yThrow keys
         float yOffset = yThrow * Time.deltaTime * controlSpeed;
         //transform.localPosition means where you currently are in the world
-        float newYPos = transform.localPosition.y + yOffset;
+        float rawYPos = transform.localPosition.y + yOffset;
+        //this will cause the rocket to not go off the screen
+        float clampedYPos = Mathf.Clamp(rawYPos, negYRange, yRange);
         
         //this is responsible for moving the player object
         //Vector3 is used if your making a 3d game
         //Vector2 is used to make 2d games
-        transform.localPosition = new Vector3 (newXPos, newYPos, transform.localPosition.z);
+        transform.localPosition = new Vector3 (clampedXPos, clampedYPos, transform.localPosition.z);
     }
 }
