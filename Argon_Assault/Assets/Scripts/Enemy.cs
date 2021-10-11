83db9169d7a9496fc8aa4d1cad91c8091052cd3b
Tahsin Ahmed <shanahmed1997@gmail.com>
2021-10-11 16:05:18 -0400

    player freeze when killing boss

--- Argon_Assault/Assets/Scripts/Enemy.cs
+++ Argon_Assault/Assets/Scripts/Enemy.cs
@@ -9,7 +9,7 @@ public class Enemy : MonoBehaviour
     [SerializeField] private GameObject deathVFX;
     [SerializeField] private GameObject hitVFX;
     [SerializeField] private int scorePerHit = 15;
-    [SerializeField] int hitpoints = 2;
+    [SerializeField] int hitpoints = 30;
     //this calls the scoreboard.cs class
     ScoreBoard scoreBoard;
 
@@ -21,29 +21,50 @@ public class Enemy : MonoBehaviour
     //this is responsible for when the particles hit something
     private void OnParticleCollision(GameObject other)
     {
-        ProcessHit();
-        if (hitpoints < 1)
+        ProcessBossHit();
+        /*
+        this is used to make sure the lazer know's it's hitting the enemy
+        if (other.CompareTag("HitMarker"))
         {
-          KillEnemy();  
+            StartCoroutine(TouchedLava());
+        }
+        */
+        if (hitpoints == 0)
+        {
+          KillEnemy();
+          stopPlayerAnimation();
         }
 
     }
 
-    private void ProcessHit()
+    //this will stop the animation, and player moving
+    private void stopPlayerAnimation()
+    {
+        var player = GameObject.FindWithTag("Player");
+        player.GetComponent<Animator>().enabled = false;
+        var playerMovement = GameObject.FindWithTag("GameController");
+        playerMovement.GetComponent<PlayerControls>().enabled = false;
+    }
+
+    //this method will react when the laser hits an object
+    private void ProcessBossHit()
     {
         //this will make the particle effect appear after object is hit
         Instantiate(hitVFX, transform.position, Quaternion.identity);
         //health system
         hitpoints = hitpoints - 1; 
         //this is using the method from the scoreboard.cs class
-        scoreBoard.IncreaseScore(scorePerHit);
+        scoreBoard.IncreaseScore(scorePerHit);  
     }
+    
+    //this method is responsible for the object being destroyed
     private void KillEnemy()
     {
+        var go = GameObject.FindWithTag("Boss");
         //this will make the particle effect appear after object is destroyed
         Instantiate(deathVFX, transform.position, Quaternion.identity);
         //it will make object disappear
-        Destroy(gameObject);
+        Destroy(go);
     }
 
     
